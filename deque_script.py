bf6f9638db5e7591840eb42b98ecb56b5b9cef97
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-12-16 20:10:23 +0100

    deque

---
+++ deque_script.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+
+def is_palindrome(string):
+    string_deque = deque(string)
+
+    while len(string_deque) > 1:
+        first = string_deque.popleft()
+        last = string_deque.pop()
+        if first != last:
+            return False
+    return True
